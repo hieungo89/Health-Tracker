727c8242db5762a363794a1e3ec0bb77d31fdf4a
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-11-15 12:19:18 -0800

    files moved, these are duplicate

--- src/js/main.jsx
+++
@@ -1,7 +0,0 @@
-import * as bootstrap from 'bootstrap'; // Import all of Bootstrap's JS
-import '../scss/styles.scss'; // Import our custom CSS
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-
-ReactDOM.render(<App/>, document.getElementById('root'));
